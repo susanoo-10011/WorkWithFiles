11497b11113497b4981860fe308b78f53879e011
susanoo-10011 <cycahosir@gmail.com>
2023-05-09 20:23:12 +0700

    task3.  исходное кол-во файлов и исходный размер

---
+++ task3/ClearFolders.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    internal static class ClearFolders
+    {
+        public static void GetData(string userInput)
+        {
+            try
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(userInput);
+                TimeSpan timeSpan = TimeSpan.FromMinutes(30);
+
+                if (!directoryInfo.Exists)
+                {
+                    Console.WriteLine("Папка не существует!");
+                    return;
+                }
+
+                foreach (FileInfo file in directoryInfo.GetFiles())
+                {
+                    if (file.LastAccessTime + timeSpan < DateTime.Now)
+                    {
+                        file.Delete();
+                        Console.WriteLine($"Удален файл: {file.FullName}");
+                    }
+                }
+
+                foreach (DirectoryInfo directory in directoryInfo.GetDirectories())
+                {
+                    if (directory.LastAccessTime + timeSpan < DateTime.Now)
+                    {
+                        directory.Delete(true);
+                        Console.WriteLine($"Удалена папка: {directory.FullName}");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при очистке папки: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Укажите путь к папке: ");
+            string path = Console.ReadLine();
+
+            var count = DataAboutFiles.CountFilesInFolder(path);
+            Console.WriteLine($"Общее количество файлов: {count}");
+
+            var size = DataAboutFiles.GetDirSize(path);
+            Console.WriteLine($"Исходный размер папки: {size} байт.");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
